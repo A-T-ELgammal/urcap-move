c9c805e5d4ec7fa9582e368ce25cd775793f0fb6
atelgammal <a.t.elgammal@gmail.com>
2024-07-09 12:23:59 +0300

    create activator & build

--- com.TulipTechnologies.SampleMoveURCap/src/main/java/com/TulipTechnologies/SampleMoveURCap/impl/Activator.java
+++ com.TulipTechnologies.SampleMoveURCap/src/main/java/com/TulipTechnologies/SampleMoveURCap/impl/Activator.java
@@ -3,14 +3,16 @@ package com.TulipTechnologies.SampleMoveURCap.impl;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
+import com.ur.urcap.api.contribution.InstallationNodeContribution;
+
 /**
  * Hello world activator for the OSGi bundle URCAPS contribution
  *
  */
 public class Activator implements BundleActivator {
 	@Override
-	public void start(BundleContext bundleContext) throws Exception {
-		System.out.println("Activator says Hello World!");
+	public void start(BundleContext context) throws Exception {
+		context.registerService(InstallationNodeContribution.class, new SimpleMoveService(),null);
 	}
 
 	@Override
